cbe2ba17f1dbf9407a8eab8264a509f7e2c222e9
Igor Muzyka <igormuzyka42@gmail.com>
2018-08-31 20:30:38 +0300

    Added Package.swift

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:4.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "TypePreservingCodingAdapter",
+    products: [
+        .library(name: "TypePreservingCodingAdapter", targets: ["TypePreservingCodingAdapter"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "TypePreservingCodingAdapter",
+            dependencies: [],
+            path: "Sources"),
+    ]
+)
