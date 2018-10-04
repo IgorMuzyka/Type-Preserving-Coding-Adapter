f045c879218d4c3a8945a9e9b99fa9cbbd4019ab
Igor Muzyka <imuz@ciklum.com>
2018-10-04 17:56:48 +0300

    Removed unneeded commented copyright notices added by Xcode

--- Tests/TypePreservingCodingAdapterTests/Models/Animal.swift
+++ Tests/TypePreservingCodingAdapterTests/Models/Animal.swift
@@ -1,10 +1,3 @@
-//
-//  Animal.swift
-//  TypePreservingAdapterTests
-//
-//  Created by Igor Muzyka on 7/21/18.
-//  Copyright © 2018 Igor Muzyka. All rights reserved.
-//
 
 public protocol Animal: Codable {
 
--- Tests/TypePreservingCodingAdapterTests/Models/Cat.swift
+++ Tests/TypePreservingCodingAdapterTests/Models/Cat.swift
@@ -1,10 +1,3 @@
-//
-//  Cat.swift
-//  TypePreservingAdapterTests
-//
-//  Created by Igor Muzyka on 7/21/18.
-//  Copyright © 2018 Igor Muzyka. All rights reserved.
-//
 
 public class Cat: Mammal {
 
